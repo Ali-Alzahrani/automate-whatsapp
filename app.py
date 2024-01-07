08d4a4459866932149b0703d6ba3d98ce1584d1c
Ali-Alzahrani <47792156+Ali-Alzahrani@users.noreply.github.com>
2024-01-07 18:33:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,22 @@ from twilio.twiml.messaging_response import MessagingResponse
 app = Flask(__name__)
 
 # Defining the app route
-@app.route("/", methods=["get", "post"])
+#@app.route("/", methods=["get", "post"])
 
 # Defining the main function
+#def reply():
+    #response = MessagingResponse()
+    #response.message("Hello")
+    #return str(response)
+
+@app.route("/", methods=["POST"])
 def reply():
+    print("Received a message!")  # Check if this message appears in the Heroku logs
     response = MessagingResponse()
     response.message("Hello")
+    print(str(response))  # Check the response being generated
     return str(response)
+
 
 # Run the application
 if __name__ == "__main__":
