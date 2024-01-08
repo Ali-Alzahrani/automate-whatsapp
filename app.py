cf613248da222d0e47fd487e6a2376d731500ccd
Ali-Alzahrani <47792156+Ali-Alzahrani@users.noreply.github.com>
2024-01-08 19:00:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from pymongo import MongoClient
 
+cluster = MongoClient("mongodb+srv://Ali:1931@cluster0.f74zbur.mongodb.net/?retryWrites=true&w=majority")
+db = cluster["Auto-parts-shop"]
+users = db["users"]
+orders = db["orders"]
+
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
@@ -10,7 +15,7 @@ def reply():
     
     print("Received a message!")  # Check if this message appears in the Heroku logs
     response = MessagingResponse()
-    response.message("Hello4")
+    response.message("Hello5")
     print(str(response))  # Check the response being generated
     return str(response)
     
