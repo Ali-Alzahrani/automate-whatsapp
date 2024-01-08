adba0e632dcdbdbf3beccaa09b1a85f9bd3ff2ff
Ali-Alzahrani <47792156+Ali-Alzahrani@users.noreply.github.com>
2024-01-09 02:48:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from pymongo import MongoClient
+from datetime import datetime
 
 cluster = MongoClient("mongodb+srv://Ali:1931@cluster0.f74zbur.mongodb.net/?retryWrites=true&w=majority")
 db = cluster["Auto-parts-shop"]
@@ -11,13 +12,29 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def reply():
+    text = request.form.get("Body")
+    number = request.form.get("From")
+    number = number.replace("whatsapp:", "")
+    res = MessagingResponse()
+    user = users.find_one({"number": number})
+
+    if bool(user) == False:
+        # The user is new
+        res.message("Hi in *China*. \n Choose from:" "\n 1️⃣ to contact us \n 2 to order \n 3 hours \n 4 address")
+        # Add this new usesr to the db
+        users.insert_one({"number": number, "status": "main", "messages": []})
+    else:
+        res.message("i don't know what to say")
+
+    users.update_one({"number": number}, {"$push": {"messages": {"text": text, "date": datetime.now()}}})
     
-    
-    print("Received a message!")  # Check if this message appears in the Heroku logs
-    response = MessagingResponse()
-    response.message("حصه احبك")
-    print(str(response))  # Check the response being generated
     return str(response)
+    
+    #print("Received a message!")  # Check if this message appears in the Heroku logs
+    #response = MessagingResponse()
+    #response.message("حصه احبك")
+    #print(str(response))  # Check the response being generated
+    #return str(response)
     
 
 
