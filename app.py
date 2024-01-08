d1c0a1abe92b1c31675c0db67a3f34218a62ec2f
Ali-Alzahrani <47792156+Ali-Alzahrani@users.noreply.github.com>
2024-01-08 18:42:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def reply():
     return("Go")
-    '''
+    """
     #print("Received a message!")  # Check if this message appears in the Heroku logs
     response = MessagingResponse()
     response.message("Hello4")
     #print(str(response))  # Check the response being generated
     return str(response)
-    '''
+    """
 
 
 # Run the application
