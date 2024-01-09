dc557eef78b16cd2b2d328f10b4b77f589af8647
Ali-Alzahrani <47792156+Ali-Alzahrani@users.noreply.github.com>
2024-01-09 14:25:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -36,16 +36,25 @@ def reply():
         elif option == 2:
             res.message("You have entered the ordering mode")
             users.update_one({"number": number}, {"$set": {"status": "ordering"}})
-            res.message("1 for Mg \n 2 for Changan")
+            res.message("0 to go to the main menu \n 1 for Mg \n 2 for Changan")
         elif option == 3:
             res.message("Here is nubmer 3")
         elif option == 4:
             res.message("Here is number 4")
         else:
             res.message("Please enter a valid response")
+            #return str(res)
+
+    elif user["status"] == "ordering":
+        try:
+            option = int(text)
+        except:
+            res.message("Please enter a valid response")
             return str(res)
             
-        
+        if option == 0:
+            users.update_one({"number": number}, {"$set": {"status": "main"}})
+            res.message("Choose from:" "\n 1️⃣ to contact us \n 2 to order \n 3 hours \n 4 address")
         
 
     # Always update the user's data by adding his new message to the array
