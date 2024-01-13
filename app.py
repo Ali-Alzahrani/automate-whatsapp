3e4c3670d4279a4061b1613d5e6c0e1664196868
Ali-Alzahrani <47792156+Ali-Alzahrani@users.noreply.github.com>
2024-01-13 14:03:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def reply():
             res.message("الرجاء تحديد نوع السياره: \n 1️⃣ ام جي \n 2️⃣ تشانجان \n 3️⃣ هافال \n 4️⃣ ماكسوس \n \n 0️⃣ للعوده للقائمه الرئيسيه")
             
         elif option == 2:
-            res.message("Here is number 2")
+            res.location(21.257466850773422, 40.452543153477315)
             
         elif option == 3:
             res.message("Here is nubmer 3")
